1199241f0e909dbf5d33905cf07b9e9dce7bba2d
MarckDmarck <155605126+MarckDmarck@users.noreply.github.com>
2024-02-21 04:23:50 +0000

    generador de nombres

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombre = ["El", "Nuestro"];
+  let adjetivo = ["grandioso", "grande"];
+  let sustantivo = ["corredor", "ardilla"];
+
+  for (let p = 0; p < pronombre.length; p++) {
+    for (let a = 0; a < adjetivo.length; a++) {
+      for (let s = 0; s < sustantivo.length; s++) {
+        let dominio = pronombre[p] + adjetivo[a] + sustantivo[s] + ".com";
+        console.log(dominio);
+      }
+    }
+  }
 };
